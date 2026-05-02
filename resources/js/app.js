a76c078bf3724c4db7b1d788d7f96e87947456e1
SkyKa <sky.ka@mail.ru>
2026-05-03 01:59:16 +1200

    feature(front): add vue support to project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-//
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './components/App.vue';
+
+const app = createApp(App);
+
+app.use(createPinia());
+
+app.mount('#app');
